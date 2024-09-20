a6c078d6594d1cb684767b474bdd270f6899ab54
Livia Correia <liviacorreiasilva492@gmail.com>
2024-09-20 17:18:05 -0300

    Linguagem Java
    Cálculo simples para reajustar um valor de saldo em 1% e exibe o saldo reajustado no console.

--- Reajuste de Saldo Bancario.java
+++ Reajuste de Saldo Bancario.java
@@ -1,4 +1,4 @@
-public class Main { // Define a classe principal chamada Main, onde  todo o código será executado a partir dessa classe.//
+public class Main { // Define a classe principal chamada Main, onde  todo o código será executado a partir dessa classe//
     public static void main(String[] args) { //metodo main onde o código dentro deste método será executado quando o programa for iniciado.//
 double saldo = 1300; //eclara uma variável chamada saldo do tipo double (armazena números com casas decimais),ela é inicializada com o valor de 1300, que representa o saldo inicial na conta//
 
